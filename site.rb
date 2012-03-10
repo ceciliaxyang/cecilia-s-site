8467015f43dec09b304a8bde5012e1917e9b68f5
Cecilia Xi Yang <ceciliaxyang@Cecilias-MacBook-Air.local>
2012-03-10 15:29:23 -0800

    changed font on css

--- site.rb
+++ site.rb
@@ -2,22 +2,26 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  erb :home
-end
-
-get '/about' do
-  @name = 'Cecilia'
-  @seconds_old = Time.now - Time.local(1987, 3, 12)
-  @age = @seconds_old / 365 / 24 / 60 / 60
-  erb :about
-end
-
-get '/work' do
   erb :work
 end
 
+get '/info' do
+  @name = 'Cecilia'
+  @seconds_old = Time.now - Time.local(1987, 3, 12)
+  @age = @seconds_old / 365 / 24 / 60 / 60
+  erb :info
+end
+
+get '/blog' do
+  erb :blog
+end
+
+get '/contact' do
+  erb :contact
+end
+
 not_found do
-  '404!'
+  erb :notfound
 end
 
   
