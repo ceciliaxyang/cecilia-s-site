64deef561bcffda0a7c177d66abb6ecb4aed703e
Cecilia Xi Yang <ceciliaxyang@Cecilias-MacBook-Air.local>
2012-03-10 22:41:00 -0800

    added prelim nav bar

--- site.rb
+++ site.rb
@@ -2,10 +2,6 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  erb :work
-end
-
-get '/info' do
   @name = 'Cecilia'
   @seconds_old = Time.now - Time.local(1987, 3, 12)
   @age = @seconds_old / 365 / 24 / 60 / 60
@@ -14,6 +10,10 @@ end
 
 get '/blog' do
   erb :blog
+end
+
+get '/work' do
+  erb :work
 end
 
 get '/contact' do
