8cc2738be1a2442f698a690419b93a726626b981
cecilia <e.oxypetalum@gmail.com>
2014-07-12 19:25:26 -0700

    updated to new site

--- site.rb
+++ site.rb
@@ -6,23 +6,52 @@ class Site < Sinatra::Base
   get '/' do
     @seconds_old = Time.now - Time.local(1987, 3, 12)
     @age = @seconds_old / 365.25 / 24 / 60 / 60
-    erb :info
+    erb :home
   end
 
-  get '/work' do
-    erb :work
+  get '/design' do
+    @show_navbar = true
+    @show_footer = true
+    erb :design
   end
 
-  get '/blogtemp' do
-    erb :blogtemp
+
+  get '/about' do
+    @show_navbar = true
+    @show_footer = true
+    erb :about
   end
 
-  get '/info' do
-    erb :info
+  get '/okl-redesign' do
+    @show_navbar = true
+    @show_footer = true
+    erb :oklredesign
   end
-  
-  get '/instagram' do
-    erb :instagram
+
+  get '/okl-directed-shopping' do
+    @show_navbar = true
+    @show_footer = true
+    erb :okldirectedshopping
+  end
+
+  get '/okl-ipad' do
+    @show_navbar = true
+    @show_footer = true
+    erb :oklipad
+  end
+
+
+  get '/jawbone-mobile' do
+    @show_navbar = true
+    @show_footer = true
+    erb :jawbonemobile
+  end
+
+
+  get '/big-jambox-buttons' do
+    @show_navbar = true
+    @show_footer = true
+    erb :bigjamboxbuttons
   end
 
   not_found do
